c1aafdf13dc1730a1d4493d3990efcd30b014950
prosenjeetshil <prosenjeetshil@gmail.com>
2025-03-02 12:52:59 +0530

    chore: configure database and run initial migrations

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -96,9 +96,13 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "idgenerator",
+        "USER": "root",
+        "PASSWORD": "root",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
     }
 }
 
